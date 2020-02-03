3a3a94559948fb25244881659ba906ef868272f2
nitink840 <59434214+nitink840@users.noreply.github.com>
2020-02-03 13:35:01 +0530

    minut changes
    port and procfile

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var app = require('./index')
-const port = process.env.port || 8080
+const port = process.env.PORT || 8080
 
 app.use('image/',(req,res)=>{
     res.send('hello fellas, this  server is working!')
 })
 
-app.listen(port,()=>{
-    console.log(' working fine with port '+ port)
+app.listen(port,()=>{ 
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); 
 })

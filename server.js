158041abf87ce1be4a56680c2ef7da5b85fff69d
nitink840 <59434214+nitink840@users.noreply.github.com>
2020-02-03 12:45:25 +0530

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var app = require('./index')
 const port = process.env.port || 8080
 
-// app.use('/',(req,res)=>{
-//     res.send('hello fellas, this  server is working!')
-// })
+app.use('image/',(req,res)=>{
+    res.send('hello fellas, this  server is working!')
+})
 
 app.listen(port,()=>{
     console.log(' working fine with port '+ port)

b3bfba948c51b7870897f9c8126fabeb4df95270
Nitin Kushwaha <nitin@metapercept.com>
2020-03-12 12:43:03 +0530

    removed handlebars

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express')
 const app = express()
-var exphbs = require("express-handlebars");
-
 
 var db = require('./db')
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 var imageRouter = require('./imageController')
-app.use('/user',imageRouter)
+app.use('/image',imageRouter)
 
 module.exports = app

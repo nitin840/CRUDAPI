a0a06ad3774be59a1804a29d2ba52eb42502100c
Nitin Kushwaha <nitin@metapercept.com>
2020-03-23 16:21:10 +0530

    small check

--- imageController.js
+++ imageController.js
@@ -142,7 +142,7 @@ router.get('/getAll',(req,res)=>{
                 message:"No records found!"
             })
         }
-    res.status(200).json(data)
+    res.status(200).json({data : data})
     })
     .catch(err => {
         res.status(500).json({
